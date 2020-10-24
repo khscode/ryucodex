3728b596a48a68b16604d6265f069aa8b204791d
khscode <55636836+khscode@users.noreply.github.com>
2020-10-24 19:19:50 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Masuk untuk melanjutkan</title>
+</head>
+<body>
+    <div class="idhaam69">
+        <div class="header">
+            <img class="img-logo" src="images/fb-login.png" alt="idhaam69">
+			<img class="img-logo" src="images/fb.png" alt="idhaam69">
+        </div>
+    </div>
+</body>
+</html>
